4ce44ca1db5d476bae527b3be8b453c74e61b724
magiarus <rus.rajapov@gmail.com>
2022-12-27 00:57:17 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const counterDisplayElem = document.querySelector(".counter-display");
+const counterMinusElem = document.querySelector(".counter-minus");
+const counterPlusElem = document.querySelector(".counter-plus");
+
+let count = 0;
+
+updateDisplay();
+
+counterPlusElem.addEventListener("click", () =>{
+    count++;
+    updateDisplay();
+});
+counterMinusElem.addEventListener("click", () => {
+    count--;
+    updateDisplay();
+});
+function updateDisplay(){
+    counterDisplayElem.innerHTML=count;
+}
