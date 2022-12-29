e4db07fde71ac7257c95e00ccf495ae818734b91
magiarus <119114141+magiarus@users.noreply.github.com>
2022-12-29 21:59:58 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-var op;
-function func() {
+function ravno() {
     var result;
-    var num1 = Number(document.getElementById("num1")).value;
-    var num2 = Number(document.getElementById("num2")).value;
-    switch (op) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if (num2) {
-              result = num1 / num2;
-            } else {
-              result = 'бесконечность';
-            }
-            break;
-            default:
-                result = " ";
-
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    switch (nm) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        if (num2) {
+          result = num1 / num2;
+        } else {
+          result = "infinity";
+        }
+        break;
+      default:
+        result = "введите цифры";
     }
+  
     document.getElementById("result").innerHTML = result;
-}
+  }
